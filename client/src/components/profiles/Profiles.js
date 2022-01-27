f3a22e8f771696029f68ad3965713240f7a2e29b
RishikeshPolasa <rishipolasa@gmail.com>
2022-01-27 12:53:00 +0530

    added Profile and Profiles component

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,14 +1,14 @@
 import React, { Fragment, useEffect } from "react";
-import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
-import ProfileItem from "./ProfileItem";
 import { getProfiles } from "../../actions/profile";
+import ProfileItem from "./ProfileItem";
 
 const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
   useEffect(() => {
     getProfiles();
-  }, []);
+  }, [getProfiles]);
 
   return (
     <Fragment>
@@ -17,21 +17,22 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
       ) : (
         <Fragment>
           <h1 className="large text-primary">Developers</h1>
-          <p classname="lead">
+          <p className="lead">
+            {" "}
             <i className="fab fa-connectdevelop"></i> Browse and connect with
-            developers
+            developers{" "}
           </p>
-          <div className="proifles">
+          <div className="profiles">
             {profiles.length > 0 ? (
               profiles.map((profile) => (
                 <ProfileItem key={profile._id} profile={profile} />
               ))
             ) : (
-              <h4>No profiles found...</h4>
+              <h4>No profiles found</h4>
             )}
           </div>
         </Fragment>
-      )}
+      )}{" "}
     </Fragment>
   );
 };
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,11 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from "../actions/types";
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE,
+  GET_PROFILES,
+  GET_REPOS,
+} from "../actions/types";
 
 const initialState = {
   profile: null,
@@ -13,9 +20,16 @@ export default function (state = initialState, action) {
 
   switch (type) {
     case GET_PROFILE:
+    case UPDATE_PROFILE:
       return {
         ...state,
         profile: payload,
+        loading: false,
+      };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
         loading: false,
       };
     case PROFILE_ERROR:
@@ -23,6 +37,7 @@ export default function (state = initialState, action) {
         ...state,
         error: payload,
         loading: false,
+        profile: null,
       };
     case CLEAR_PROFILE: {
       return {
@@ -32,6 +47,12 @@ export default function (state = initialState, action) {
         loading: false,
       };
     }
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
+        loading: false,
+      };
     default:
       return state;
   }
